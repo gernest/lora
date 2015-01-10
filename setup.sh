e614d53d20de5e14a28020522b35ccf51726b4d6
gernest <geofreyernest@live.com>
2015-01-10 16:38:29 +0300

    Fix scripts

--- setup.sh
+++ setup.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Copyright 2015 Geofrey Ernest a.k.a gernest, All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"): you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 
 #if dumb terminal (file browser) run xterm so basicaly the script must run on almost any distro
 if [ $TERM == "dumb" ]; then xterm -hold -e $0; fi
