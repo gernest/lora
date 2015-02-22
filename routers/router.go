dbf194287f48dbec2f0c14b76eb3173f20ff52f6
gernest <geofreyernest@live.com>
2015-02-22 13:57:40 +0300

    Fix router

--- routers/router.go
+++ routers/router.go
@@ -24,45 +24,52 @@ func init() {
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/notice", &controllers.MainController{}, "get:Notice")
 
-	// Accounts
-	beego.Router("/accounts", &controllers.AccountController{}, "*:Index")
-	beego.Router("/accounts/register", &controllers.AccountController{}, "get,post:Register")
-	beego.Router("/accounts/login", &controllers.AccountController{}, "get,post:Login")
-	beego.Router("/accounts/logout", &controllers.AccountController{}, "get:Logout")
+	ns := beego.NewNamespace("/web",
+		beego.NSBefore(filters.AuthLevelSix),
+		beego.NSNamespace("/accounts",
+			beego.NSRouter("/", &controllers.AccountController{}, "*:Index"),
+			beego.NSRouter("/register", &controllers.AccountController{}, "get,post:Register"),
+			beego.NSRouter("/login", &controllers.AccountController{}, "get,post:Login"),
+			beego.NSRouter("/logout", &controllers.AccountController{}, "get:Logout"),
+			beego.NSNamespace("/profile",
+				beego.NSRouter("/:profileID:int/edit", &controllers.ProfileController{}, "get,post:Edit"),
+				beego.NSRouter("/:profileID:int/view", &controllers.ProfileController{}, "get:Display"),
+			),
+		),
+		beego.NSNamespace("/project",
+			beego.NSRouter("/new", &controllers.ProjectController{}, "get,post:NewProject"),
+			beego.NSRouter("/:id:int/update/", &controllers.ProjectController{}, "get,post:Update"),
+			beego.NSRouter("/:id:int/delete", &controllers.ProjectController{}, "get,post:Remove"),
+			beego.NSRouter("/:id:int/preview", &controllers.ProjectController{}, "get,post:Preview"),
+			beego.NSRouter("/:id:int/deploy", &controllers.ProjectController{}, "get,post:Deploy"),
+			beego.NSRouter("/list", &controllers.ProjectController{}, "get:List"),
+		),
+		beego.NSNamespace("/page",
+			beego.NSRouter("/:projectID:int/:pageID:int/update", &controllers.PageController{}, "get,post:Update"),
+			beego.NSRouter("/:projectID:int/:pageID:int/preview", &controllers.PageController{}, "get,post:Preview"),
+		),
+		beego.NSNamespace("/section",
+			beego.NSRouter("/:pageID:int/:sectionID:int/update", &controllers.SectonController{}, "get,post:Update"),
+		),
+		beego.NSNamespace("/subsection",
+			beego.NSRouter("/subsection/:sectionID:int/:subSectionID:int/update", &controllers.SubSectionController{}, "get,post:Update"),
+		),
+	)
 
-	// Profile
-	beego.Router("/profile/:profileID:int/edit", &controllers.ProfileController{}, "get,post:Edit")
-	beego.Router("/profile/:profileID:int/view", &controllers.ProfileController{}, "get:Display")
+	nsLora := beego.NewNamespace("/site",
+		beego.NSNamespace("/pages",
+			beego.NSRouter("/pricing", &controllers.LoraController{}, "get:Pricing"),
+			beego.NSRouter("/services", &controllers.LoraController{}, "get:Services"),
+			beego.NSRouter("/contacts", &controllers.LoraController{}, "get:Contacts"),
+			beego.NSRouter("/terms", &controllers.LoraController{}, "get:Terms"),
+			beego.NSRouter("/legal", &controllers.LoraController{}, "get:Legal"),
+			beego.NSRouter("/help", &controllers.LoraController{}, "get:Help"),
+			beego.NSRouter("/companies", &controllers.LoraController{}, "get:Companies"),
+		),
+	)
 
-	// Projects
-	beego.Router("/project/new", &controllers.ProjectController{}, "get,post:NewProject")
-	beego.Router("/project/:id:int/update/", &controllers.ProjectController{}, "get,post:Update")
-	beego.Router("/project/:id:int/delete", &controllers.ProjectController{}, "get,post:Remove")
-	beego.Router("/project/:id:int/preview", &controllers.ProjectController{}, "get,post:Preview")
-	beego.Router("/project/:id:int/deploy", &controllers.ProjectController{}, "get,post:Deploy")
-	beego.Router("/projects/list", &controllers.ProjectController{}, "get:List")
+	beego.AddNamespace(ns)
+	beego.AddNamespace(nsLora)
 
-	// Pages
-	beego.Router("/page/:projectID:int/:pageID:int/update", &controllers.PageController{}, "get,post:Update")
-	beego.Router("/page/:projectID:int/:pageID:int/preview", &controllers.PageController{}, "get,post:Preview")
-
-	// Section
-	beego.Router("/section/:pageID:int/:sectionID:int/update", &controllers.SectonController{}, "get,post:Update")
-
-	// Subsection
-	beego.Router("/subsection/:sectionID:int/:subSectionID:int/update", &controllers.SubSectionController{}, "get,post:Update")
-
-	// Lora
-	beego.Router("/pricing", &controllers.LoraController{}, "get:Pricing")
-	beego.Router("/services", &controllers.LoraController{}, "get:Services")
-	beego.Router("/contacts", &controllers.LoraController{}, "get:Contacts")
-	beego.Router("/terms", &controllers.LoraController{}, "get:Terms")
-	beego.Router("/legal", &controllers.LoraController{}, "get:Legal")
-	beego.Router("/help", &controllers.LoraController{}, "get:Help")
-	beego.Router("/companies", &controllers.LoraController{}, "get:Companies")
-
-	// clearance
-	cls := filters.NewBaseClearance()
-	cls.Register(filters.NewUser("xshabe"), filters.LEVEL_SIX, "/")
-	cls.ClearUp()
+	beego.InsertFilter("/", beego.BeforeRouter, filters.AuthLevelSix)
 }
